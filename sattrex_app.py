ea1b5b345379c5ad85c3fdb9f5efba0b98a5d166
Shivi99-Mumbai <shivangigupta9132@gmail.com>
2026-04-26 17:35:35 +0530

    Update sattrex_app.py

--- sattrex_app.py
+++ sattrex_app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 # --- PAGE CONFIG ---
-st.set_page_config(page_title="Sattrex Capital VIP Portal", layout="wide")
+st.set_page_config(page_title="Sattrex Capital Portal", layout="wide")
 
 # --- CUSTOM CSS FOR FINTECH LOOK ---
 st.markdown("""
